6eb1e74b365b2d82ac163f4a264dafb2fa0a4ac2
Tyler Payne <154254574+Yhento@users.noreply.github.com>
2024-02-19 12:39:11 -0600

    finished exercises.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+  celsius = parseFloat(((f-32) * (5/9)).toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  farenheit = parseFloat((c * 9/5 + 32).toFixed(1));
+  return farenheit;
 };
+
 
 // Do not edit below this line
 module.exports = {
