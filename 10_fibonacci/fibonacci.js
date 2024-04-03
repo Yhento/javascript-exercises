682d0bef4c9d89ac84b193ec5aa1f3f8bd3abb8f
Tyler Payne <154254574+Yhento@users.noreply.github.com>
2024-04-03 09:36:03 -0500

    finish fibonacci exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    arr = [];
+
+    if (num < 0) {
+        return "OOPS";
+    } 
+
+    if (num == 0) {
+        return 0;
+    }
+
+    if (typeof num === String) {
+        num = parseInt(num);
+    }
+
+    for (i=0; i<num; i++) {
+        if (i == 0 || i == 1){
+            arr.push(1);
+        }
+        else {
+            running_total = arr[i-1] + arr[i-2]
+            arr.push(running_total);
+        }
+    }
+
+    return arr[num-1];
 
 };
+
+
+console.log(fibonacci("25"))
 
 // Do not edit below this line
 module.exports = fibonacci;
