32a88ea90ff63a6689aa1b557787506e12427791
darth <154254574+Yhento@users.noreply.github.com>
2024-04-08 11:31:18 -0500

    complete plaindrome assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  original_string = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  reverse_string = original_string.split("").reverse().join("");
+
+
+
+
+  
+
+  
+
+   
+  let is_palindrome = original_string == reverse_string ? true : false;
+
+  return is_palindrome;
+
+  
+
+
 
 };
+
+palindromes("A car, a man, a maraca.");
+
 
 // Do not edit below this line
 module.exports = palindromes;
