80963fe79089a71a90bbfb756305dced9d35dc3c
darth <154254574+Yhento@users.noreply.github.com>
2024-04-08 08:27:05 -0500

    finish calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -4,25 +4,55 @@ const add = function(num1, num2) {
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+
+  return num1 - num2;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+
+  return arr.reduce((total, num) => total + num, 0);
 	
 };
 
-const multiply = function() {
+
+
+const multiply = function(arr) {
+
+  return arr.reduce((total, num) => total * num)
 
 };
 
-const power = function() {
+
+
+const power = function(num1, num2) {
+
+  return num1 ** num2;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+
+  factorial_array = [];
+
+  if (num == 0) {
+    num = 1;
+  }
+
+  for (let i = 1; i <= num; i++) {
+        factorial_array.push(i);
+  }
+
+  ;
+
+  factorial_array.reverse();
+
+  return factorial_array.reduce((total, num) => total * num);
 	
 };
+
+
 
 // Do not edit below this line
 module.exports = {
